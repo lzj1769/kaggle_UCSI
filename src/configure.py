f57512a5b0753dd2ef5205a1e3b14e8fd10025a6
lzj1769 <lzj1769@gmail.com>
2019-08-27 16:17:20 +0200

    add

---
+++ src/configure.py
@@ -0,0 +1,14 @@
+# input data configuration
+SPLIT_FOLDER = "/home/rs619065/kaggle/UCSI/input/understanding_cloud_organization/split"
+DATA_FOLDER = "/home/rs619065/kaggle/UCSI/input/understanding_cloud_organization/train_images"
+TRAIN_DF_PATH = "/home/rs619065/kaggle/UCSI/input/understanding_cloud_organization/train.csv"
+
+# imagenet pretrained model configuration
+MEAN = [0.485, 0.456, 0.406]
+STD = [0.229, 0.224, 0.225]
+
+# model weights configuration
+SAVE_MODEL_PATH = "/home/rs619065/kaggle/UCSI/input/models"
+
+# training history
+TRAINING_HISTORY_PATH = "/home/rs619065/kaggle/UCSI/history"
